f30325936c64543cdaae55e919dd3ae27fd334e1
elliewetzel <40366109+elliewetzel@users.noreply.github.com>
2018-06-28 12:01:38 -0700

    Level 0, Module 2. Intro to Random.

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -13,17 +13,16 @@ public class IntroToRandom {
 		//3. Now make r random. 
 		//   Create an object of the Random class
 		//	 use .nextInt() to get a random number
-		
-
+		r = new Random().nextInt(101);
 		//4. Limit the random number between 0 and 100
-
+		
 		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
-
+		r = new Random().nextInt((76 - 24)+1) + 24;
 		//6. Challenge: Limit the random number between -222 and 88
 
 		//1. Print out the value of r
-		
-		//2. Run the program.  What number appears in the console?
-		//   Run it again. Is the number the same?
+		System.out.println(r);
+		//2. Run the program.  What number appears in the console? 0
+		//   Run it again. Is the number the same? yes
 	}
 }
